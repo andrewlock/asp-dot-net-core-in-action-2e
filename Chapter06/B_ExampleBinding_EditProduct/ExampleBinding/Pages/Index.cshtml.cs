429957d2d841bfaf9d535d7c75421940cc91e76c
Andrew Lock <andrewlock.net@gmail.com>
2020-11-10 22:18:46 +0000

    Update Chapter 6 to .NET 5

--- Chapter06/B_ExampleBinding_EditProduct/ExampleBinding/Pages/Index.cshtml.cs
+++ Chapter06/B_ExampleBinding_EditProduct/ExampleBinding/Pages/Index.cshtml.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace ExampleBinding.Pages
 {
@@ -12,18 +12,15 @@ namespace ExampleBinding.Pages
     public class IndexModel : PageModel
     {
         public ProductModel Product { get; set; }
-
         public RedirectToPageResult OnGet()
         {
             return RedirectToPage("EditProduct");
-
         }
 
         public void OnPostEditProduct(ProductModel product)
         {
             Product = product;
         }
-
         public void OnPostEditTwoProducts(ProductModel product1, ProductModel product2)
         {
             Product = product1;
