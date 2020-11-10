ee1f12da1bb65fa17ffec679bf66026d1706f733
Andrew Lock <andrewlock.net@gmail.com>
2020-11-10 22:03:14 +0000

    Update Chapter 5 to .NET 5

--- Chapter05/A_RoutingExamples/RoutingExample/Startup.cs
+++ Chapter05/A_RoutingExamples/RoutingExample/Startup.cs
@@ -1,15 +1,14 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RoutingExample
 {
