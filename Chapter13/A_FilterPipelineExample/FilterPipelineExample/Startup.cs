aeccf41606757b1e889daf8f67c546530706a6c7
Andrew Lock <andrewlock.net@gmail.com>
2020-11-12 19:26:10 +0000

    Update Chapter 13 to .NET 5

--- Chapter13/A_FilterPipelineExample/FilterPipelineExample/Startup.cs
+++ Chapter13/A_FilterPipelineExample/FilterPipelineExample/Startup.cs
@@ -1,16 +1,14 @@
+using FilterPipelineExample.Filters;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using FilterPipelineExample.Filters;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace FilterPipelineExample
 {
