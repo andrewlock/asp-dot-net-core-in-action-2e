534a3e065ae439898cb13f823d46120f4f4696c6
Andrew Lock <andrewlock.net@gmail.com>
2020-11-12 18:59:12 +0000

    Update Chapter 12 to .NET 5

--- Chapter12/A_InstallEFCore/RecipeApplication/Startup.cs
+++ Chapter12/A_InstallEFCore/RecipeApplication/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +6,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RecipeApplication.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RecipeApplication
 {
