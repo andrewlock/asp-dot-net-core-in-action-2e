677bd6f1c93d3a9ee175fd880808aa01335f1118
Andrew Lock <andrewlock.net@gmail.com>
2020-11-12 18:26:18 +0000

    Update Chapter 11 to .NET 5

--- Chapter11/C_ReplacingDefaultConfigProviders/ReplacingDefaultConfigurationProviders/Pages/Index.cshtml.cs
+++ Chapter11/C_ReplacingDefaultConfigProviders/ReplacingDefaultConfigurationProviders/Pages/Index.cshtml.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ReplacingDefaultConfigurationProviders.Pages
 {
--- Chapter11/D_UsingDifferentEnvironments/UsingDifferentEnvironments/Pages/Index.cshtml.cs
+++ Chapter11/D_UsingDifferentEnvironments/UsingDifferentEnvironments/Pages/Index.cshtml.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace UsingDifferentEnvironments.Pages
 {
