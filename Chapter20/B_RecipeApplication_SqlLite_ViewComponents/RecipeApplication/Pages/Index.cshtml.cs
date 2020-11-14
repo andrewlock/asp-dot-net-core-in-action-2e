6e22c543ff367fabd80f50249b4762b38f77df47
Andrew Lock <andrewlock.net@gmail.com>
2020-11-14 22:18:14 +0000

    Update Chapter 20 to .NET 5

--- Chapter20/B_RecipeApplication_SqlLite_ViewComponents/RecipeApplication/Pages/Index.cshtml.cs
+++ Chapter20/B_RecipeApplication_SqlLite_ViewComponents/RecipeApplication/Pages/Index.cshtml.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using RecipeApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RecipeApplication.Pages
 {
