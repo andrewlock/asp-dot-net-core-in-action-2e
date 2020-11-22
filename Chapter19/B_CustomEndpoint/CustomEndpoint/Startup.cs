3c02f51c2688cd060ee5910b47a92d7223c141ea
Andrew Lock <andrewlock.net@gmail.com>
2020-11-22 14:45:15 +0000

    Add lightweight endpoint example to chapter 19

--- Chapter19/B_CustomEndpoint/CustomEndpoint/Startup.cs
+++ Chapter19/B_CustomEndpoint/CustomEndpoint/Startup.cs
@@ -1,9 +1,11 @@
 using CustomEndpoint.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -64,6 +66,9 @@ namespace CustomEndpoint
                 endpoints.MapVersion("/version");
                 endpoints.MapPingPong("/ping");
 
+                //endpoints.MapGet("/ping", (HttpContext ctx) => 
+                //    ctx.Response.WriteAsync("pong"));
+
                 // Example of using route parameters in endpoint routes
                 endpoints
                     .MapMiddleware<CalculatorMiddleware>("/add/{a}/{b}")
@@ -72,7 +77,25 @@ namespace CustomEndpoint
                 // Requires authorization (must be logged in)
                 endpoints.MapHealthChecks("/healthz")
                     .RequireAuthorization();
+
+                endpoints.MapGet("/isEven/{value:int}", context => {
+                    int value = int.Parse((string)context.GetRouteValue("value"));
+                    bool isEven = value % 2 == 0;
+                    return context.Response.WriteAsync(isEven ? "even" : "odd");
+                });
+
+                endpoints.MapGet("/status", async context => {
+                    var status = new { Running = true };
+                    await context.Response.WriteAsJsonAsync(status);
+                });
+
+                endpoints.MapPost("/echo", async context => {
+                    var json = await context.Request.ReadFromJsonAsync<MyCustomType>();
+                    await context.Response.WriteAsJsonAsync(json);
+                });
             });
         }
     }
+    public class MyCustomType
+    { }
 }
